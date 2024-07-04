49c042fd820d4ae2c13e17dfc69f99f6f22d58aa
Ayush Kumar Singh <ayushsingh16200@gmail.com>
2024-07-04 12:30:32 +0530

    login auth

--- demo/src/main/java/com/example/demo/controller/userController.java
+++ demo/src/main/java/com/example/demo/controller/userController.java
@@ -1,3 +1,48 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.pojo.User;
+import com.example.demo.service.userService;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1/users")
 public class userController {
-    
+    @Autowired
+    private userService userService;
+    @PostMapping("/records")
+	   public ResponseEntity<?> addData(@RequestBody User obj) {
+	       if (userService.existsByEmail(obj.getEmail())) {
+	           return ResponseEntity.status(HttpStatus.CONFLICT).body("Email already exists!");
+	       }
+	       User createdUser = userService.createNewRecordService(obj);
+	       return ResponseEntity.ok(createdUser);
+	   }
+   @GetMapping("/records")
+   public List<User> getAllData() {
+       return userService.getAllRecordsService();
+   }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody User userData) {
+        System.out.println(userData);
+        User user = userService.findByEmail(userData.getEmail());
+        if (user != null && user.getPassword().equals(userData.getPassword())) {
+            System.out.println(user);
+            return ResponseEntity.ok(user);
+        }
+        return (ResponseEntity<?>) ResponseEntity.internalServerError();
+    }
 }
--- demo/src/main/java/com/example/demo/service/userService.java
+++ demo/src/main/java/com/example/demo/service/userService.java
@@ -1,3 +1,14 @@
-public class userService {
-    
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.pojo.User;
+
+public interface userService {
+    User createNewRecordService(User user);
+	List<User> getAllRecordsService();
+	Optional<User> getOneRecordService(int id);
+	User findByEmail(String email);
+	boolean existsByEmail(String email);
 }
